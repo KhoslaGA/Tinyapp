9595fdc8d53c0bc35b1dffbe88f0e995d444d344
Gautam Khosla <gautamkhosla75@gmail.com>
2023-05-05 21:00:36 -0400

    Created database file

--- database.js
+++ database.js
@@ -2,11 +2,6 @@ const urlDatabase = {};
 //database helper file
 const users = {};
 
-app.set("view engine", "ejs");
-// app.use(express.static('public'));
-app.use(express.static('public', () => {
-    console.log('Static assets middleware called');
-}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
